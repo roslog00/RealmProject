5e8617736f6b899e46247297338856210fbb8e2f
roslog00 <135430018+roslog00@users.noreply.github.com>
2023-06-27 14:39:53 +0300

    Initial Commit

---
+++ RealmProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RealmProject
+//
+//  Created by Вероника Гера on 27.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RealmProject/RealmProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmProjectApp.swift
+//  RealmProject
+//
+//  Created by Вероника Гера on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RealmProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
